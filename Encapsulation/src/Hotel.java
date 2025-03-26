8aa22f2aedd7932537e9f3604914b5d6ece5ed05
supriyakp5 <supriyakp696@gmail.com>
2025-03-26 13:26:27 +0530

    p

---
+++ Encapsulation/src/Hotel.java
@@ -0,0 +1,28 @@
+public class Hotel {
+
+    private String name;
+    private String area;
+
+    public Hotel(String name,String area)
+    {
+        this.name=name;
+        this.area=area;
+        System.out.println("encapsulation");
+    }
+    public String getName()
+    {
+         return name;
+    }
+    public String getArea()
+    {
+        return area;
+    }
+    public void setName(String name)
+    {
+        this.name=name;
+    }
+    public void setArea(String area)
+    {
+        this.area=area;
+    }
+}
---
+++ Encapsulation/src/Runnhotel.java
@@ -0,0 +1,9 @@
+public class Runnhotel {
+
+    public static void main(String[] args){
+        Hotel hotel=new Hotel("udupi","RRNagar");
+        System.out.println("hotel:"+hotel.getName());
+        System.out.println("hotel:"+hotel.getArea());
+    }
+
+}
